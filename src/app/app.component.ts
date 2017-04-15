36aa0ad06070f8885f21f273f73e531faa782977
Menuka Warushavithana <menuka.14@cse.mrt.ac.lk>
2017-04-16 00:57:55 +0530

    Implement operations on FirebaseListObservabls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {AngularFire, FirebaseListObservable, FirebaseObjectObservable} from "angularfire2";
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +10,37 @@ import {AngularFire, FirebaseListObservable, FirebaseObjectObservable} from "ang
 export class AppComponent {
   title = 'app works!';
 
+  courses$ : FirebaseListObservable<any>;
+  lesson$ : FirebaseObjectObservable<any>;
+  firstCourse: any;
+
+
   constructor(private af: AngularFire){
-    const courses$: FirebaseObjectObservable<any> = af.database.object('courses');
-    courses$.subscribe(val => console.log(val));
+    this.courses$ = af.database.list('courses');
+    this.courses$.subscribe(value => console.log(value));
+
+    this.lesson$ = af.database.object('lessons/-Khmb1aj6X2hQdfDVh0k');
+    this.lesson$.subscribe(value => console.log(value));
+
+    this.courses$.map(courses => courses[0])
+        .subscribe(
+            course => this.firstCourse = course
+        );
+  }
+
+  listPush(){
+    this.courses$.push({description: "Test New Course"})
+        .then(
+            () => console.log('List Push done'),
+            err => console.error(err)
+        );
+  }
+
+  listRemove(){
+    this.courses$.remove(this.firstCourse);
+  }
+
+  listUpdate(){
+    this.courses$.update(this.firstCourse, {description: 'Angular 2 HTTP Modified'});
   }
 }
